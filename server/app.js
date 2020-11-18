2731a4fe58b6c8cd3649378b07cdef529aed1069
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2020-11-18 10:20:18 +0000

    Chore: setup basic server

---
+++ server/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || '3100';
+
+// logger
+app.use(morgan('dev'));
+
+// 3rd party middleware
+app.use(cors());
+
+// configure bodyParser
+app.use(bodyParser.json());
+
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  console.log(err);
+  if (err) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.send((500, err));
+  }
+});
+
+
+app.listen(port || 3000, () => {
+  console.log(`Started on port ${port}`);
+});
+
+export default app;
