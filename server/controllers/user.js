45a6912774f63594d270abca0838dd1e2af4ff59
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2020-11-18 18:38:38 +0000

    Feature: add search opportunities

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,6 +39,20 @@ class userController {
       res.status(500).json(error);
     }
   }
+
+  static async searchOpportunities(req, res) {
+    try {
+      const query = {
+        offset: req.offset || 0,
+        size: req.page || 0,
+      };
+      const getOpportunities = await processor.getOpportunities(query, req.body);
+      res.status(200).json(getOpportunities);
+    } catch (error) {
+      logger.error(error);
+      res.status(500).json(error);
+    }
+  }
 }
 
 export default userController;
