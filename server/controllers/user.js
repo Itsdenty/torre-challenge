0ade7c7917307181313f22ba4549927655662088
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2020-11-18 18:26:55 +0000

    feature: add people search

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -25,6 +25,20 @@ class userController {
       res.status(500).json(error);
     }
   }
+
+  static async searchPeople(req, res) {
+    try {
+      const query = {
+        offset: req.offset || 0,
+        size: req.page || 0,
+      };
+      const getPeople = await processor.getPeople(query, req.body);
+      res.status(200).json(getPeople);
+    } catch (error) {
+      logger.error(error);
+      res.status(500).json(error);
+    }
+  }
 }
 
 export default userController;
